6e413049274bcd061f011e513e825b65f29aa3d8
zahraAfa <anisafatimahazzahra@gmail.com>
2023-09-14 11:51:42 +0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Anisa Fatimah Azzahra on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
